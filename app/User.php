fc2e78be55dfac9425e8a76de2f81092e8606488
Justin Fidelis <justinoneil.fidelis0817@gmail.com>
2019-01-06 02:20:25 +0800

    Add author info and question created date in question item

--- app/User.php
+++ app/User.php
@@ -32,6 +32,12 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Question');
     }
+
+    public function getUrlAttribute()
+    {
+        // return route("questions.show", $this->id);
+        return '#';
+    }
     
     
 }
